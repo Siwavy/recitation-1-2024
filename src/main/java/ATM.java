d42079b25fbd4d0a410893a7a259237a65a2baf9
sjevans21 <evanssihon@gmail.com>
2024-09-12 17:28:28 -0400

    COMMIT

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,20 +1,56 @@
 import java.util.Scanner;
 
 public class ATM {
+
     /**
-     * prints the amount to dispense in twenties, tens, fives,
+     * Prints the amount to dispense in twenties, tens, fives,
      * ones, quarters, dimes, nickels, and pennies.
      *
+     * @param amount The amount in dollars.
      */
-    public static void dispense(double amount)
-    {
-        // TODO: replace this line with your code
+    public static void dispense(double amount) {
+        int intAmount = (int) amount;
+        int twenty = intAmount / 20;
+        int remainderOfTwenty = intAmount % 20;
+
+        int ten = remainderOfTwenty / 10;
+        int remainderOfTen = remainderOfTwenty % 10;
+
+        int five = remainderOfTen / 5;
+        int remainderOfFive = remainderOfTen % 5;
+
+
+
+
+        double totalCents = amount * 100;
+        double doubleCents = totalCents - (intAmount*100);
+        int cents = (int) doubleCents;
+
+
+        int quarters = cents / 25;
+        int remainderOfQuarters = cents % 25;
+
+        int dimes = remainderOfQuarters / 10;
+        int remainderOfDimes = remainderOfQuarters % 10;
+
+        int nickels = remainderOfDimes / 5;
+        int pennies = remainderOfDimes % 5;
+
+
+        System.out.println(amount + " contains:");
+        System.out.println(twenty + " in $20");
+        System.out.println(ten + " in $10");
+        System.out.println(five + " in $5");
+        System.out.println(remainderOfFive + " in $1");
+        System.out.println(quarters + " in 25¢");
+        System.out.println(dimes + " in 10¢");
+        System.out.println(nickels + " in 5¢");
+        System.out.println(pennies + " in 1¢");
     }
 
-    // DO NOT TOUCH THE CODE BELOW THIS LINE
-    public static void main(String [] arg) {
-        Scanner scn = new Scanner(System.in);
 
+    public static void main(String[] arg) {
+        Scanner scn = new Scanner(System.in);
         double amount = scn.nextDouble();
         dispense(amount);
     }
